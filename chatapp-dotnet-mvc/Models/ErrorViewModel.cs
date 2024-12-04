32d02f4e701b27d40c678eeda4a39af1a976a46c
psyma <rickbengimeda@gmail.com>
2024-12-04 09:51:13 +0800

    Initial commit

---
+++ chatapp-dotnet-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace chatapp_dotnet_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
